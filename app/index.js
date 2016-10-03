c4b7a810408cbc245daf7b6b02c5bf99c5b4e283
Rishabh Bhatia <rishabh.bhatia08@gmail.com>
2016-10-04 00:25:44 +0530

    Reading files from server directory

--- app/index.js
+++ app/index.js
@@ -1,5 +1,14 @@
 // app/index.js
 const calc = require('./calc')
+const fs = require('fs')
+
+fs.readFile('./app/hello.txt', 'utf-8', function (err, content) {  
+  if (err) {
+    return console.log(err)
+  }
+
+  console.log(content)
+})
 
 const numbersToAdd = [  
   3,
@@ -9,4 +18,4 @@ const numbersToAdd = [
 ]
 
 const result = calc.sum(numbersToAdd)  
-console.log(`The result is: ${result}`)
+console.log(`The result is: ${result}`)		
