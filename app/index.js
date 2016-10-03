4432de7f3a47443f566d6ab0ceb43abe504e8af8
Rishabh Bhatia <rishabh.bhatia08@gmail.com>
2016-10-04 00:43:35 +0530

    Node server listen to request's on localhost

--- app/index.js
+++ app/index.js
@@ -1,8 +1,10 @@
 // app/index.js
 const calc = require('./calc')
 const fs = require('fs')
+const http = require('http')
+const port = 3000
 
-fs.readFile('./app/hello.txt', 'utf-8', function (err, content) {  
+/*fs.readFile('./app/hello.txt', 'utf-8', function (err, content) {  
   if (err) {
     return console.log(err)
   }
@@ -18,4 +20,19 @@ const numbersToAdd = [
 ]
 
 const result = calc.sum(numbersToAdd)  
-console.log(`The result is: ${result}`)		
+console.log(`The result is: ${result}`)	*/	
+
+const requestHandler = (request, response) => {  
+  console.log(request.url)
+  response.end('Hello Node.js Server!')
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {  
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
