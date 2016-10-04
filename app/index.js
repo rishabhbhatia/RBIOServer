3f36f854c4c9061c2bbde756b9c1bb1383410a6c
Rishabh Bhatia <rishabh.bhatia08@gmail.com>
2016-10-04 23:15:08 +0530

    Server adding request level interceptors

--- app/index.js
+++ app/index.js
@@ -1,19 +1,28 @@
-const express = require('express')  
-const app = express()  
+const express = require('express')
+const app = express()
 const port = 3000
-
-app.get('/', (request, response) => {  
-  response.send('Hello from Express!')
+app.use((request, response, next) => {
+    console.log(request.headers)
+    console.log("111")
+    next()
 })
-
-app.get('/home', (request, response) => {  
-  response.send('Welcome home!')
+app.use((request, response, next) => {
+    request.name = "Rishabh Bhatia"
+    console.log("222")
+    next()
 })
-
-app.listen(port, (err) => {  
-  if (err) {
-    return console.log('something bad happened', err)
-  }
-
-  console.log(`server is listening on ${port}`)
+app.get('/', (request, response) => {
+    console.log("333")
+    response.json({
+        lead: request.name
+    })
+})
+app.get('/home', (request, response) => {
+    response.send('Welcome home!')
+})
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+    console.log(`server is listening on ${port}`)
 })
