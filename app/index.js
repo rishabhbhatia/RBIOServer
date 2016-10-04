03ec8cd02f26bf09285d471ca3b666a40f7da565
Rishabh Bhatia <rishabh.bhatia08@gmail.com>
2016-10-04 23:17:52 +0530

    Server error handler

--- app/index.js
+++ app/index.js
@@ -3,22 +3,24 @@ const app = express()
 const port = 3000
 app.use((request, response, next) => {
     console.log(request.headers)
-    console.log("111")
     next()
 })
 app.use((request, response, next) => {
     request.name = "Rishabh Bhatia"
-    console.log("222")
     next()
 })
 app.get('/', (request, response) => {
-    console.log("333")
     response.json({
         lead: request.name
     })
 })
 app.get('/home', (request, response) => {
     response.send('Welcome home!')
+})
+app.use((err, request, response, next) => {  
+  // log the error, for now just console.log
+  console.log(err)
+  response.status(400).send('Something broke!')
 })
 app.listen(port, (err) => {
     if (err) {
